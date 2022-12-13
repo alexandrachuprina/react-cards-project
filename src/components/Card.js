2d0211a7ce8e4151f1c89b948bfd5c7548b85b1d
shplvk <114808997+shplvk@users.noreply.github.com>
2022-12-14 04:41:09 +0600

    Scale effect is added

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ export default function Card(props) {
             <img className="card--img" src={props.img} alt="Photography" />
             <div className="card--descriprion">
                 <ul className="card--info">
-                    <li>{props.name}</li>
+                    <li className="card--name">{props.name}</li>
                     <li>Author: {props.author}</li>
                     <li>Year: {props.year}</li>
                     {/* <li>Location: {props.location}</li> */}
